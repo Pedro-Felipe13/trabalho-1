ed12c5058f2acfe5b3d451fdc1ae519cb78d8a42
Pedro-Felipe13 <167812032+Pedro-Felipe13@users.noreply.github.com>
2024-06-25 16:20:50 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabalho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
